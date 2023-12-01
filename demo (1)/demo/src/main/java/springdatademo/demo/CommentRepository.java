19544274960acb535da1d3fd365e9c35141040e6
jeuunKim <ygvhn753@naver.com>
2023-12-01 14:53:31 +0900

    study

---
+++ demo (1)/demo/src/main/java/springdatademo/demo/CommentRepository.java
@@ -0,0 +1,12 @@
+package springdatademo.demo;
+
+import org.springframework.data.repository.RepositoryDefinition;
+
+import java.util.List;
+
+@RepositoryDefinition(domainClass = Comment.class, idClass = Long.class )
+public interface CommentRepository {
+    Comment save(Comment comment);
+
+    List<Comment> findAll();
+}
--- demo (1)/demo/src/main/java/springdatademo/demo/JpaRunner.java
+++ demo (1)/demo/src/main/java/springdatademo/demo/JpaRunner.java
@@ -1,8 +1,5 @@
 package springdatademo.demo;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.persistence.TypedQuery;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
@@ -10,7 +7,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Transactional
@@ -22,18 +19,22 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Post post3 = new Post();
-        post3.setTitle("hi");
+//        Post post3 = new Post();
+//        post3.setTitle("hi");
+//
+//        Comment comment = new Comment();
+//        comment.setComment("nice 2 meet you");
 
-        Comment comment = new Comment();
-        comment.setComment("nice 2 meet you");
-
-        postRepository.save(post3);
+//        postRepository.save(post3);
 
         postRepository.findAll().forEach(p ->
                 log.info("post = {}", p)
         );
 
+        Optional<Post> postById = postRepository.findById((long)321);
+
+        System.out.println(postById.isEmpty());
+        System.out.println(postById);
 
 
     }
